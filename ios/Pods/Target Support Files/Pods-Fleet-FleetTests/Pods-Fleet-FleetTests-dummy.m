efab136e516bd9a00f3e0443bec8803593fc5254
Zachary Shenkman <yankeedude02@gmail.com>
2022-02-08 15:56:44 -0500

    Renamed to Fleet

---
+++ ios/Pods/Target Support Files/Pods-Fleet-FleetTests/Pods-Fleet-FleetTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fleet_FleetTests : NSObject
+@end
+@implementation PodsDummy_Pods_Fleet_FleetTests
+@end
--- ios/Pods/Target Support Files/Pods-TapRide/Pods-TapRide-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TapRide : NSObject
-@end
-@implementation PodsDummy_Pods_TapRide
-@end
