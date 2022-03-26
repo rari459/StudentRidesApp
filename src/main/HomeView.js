24ec621b5e7eb856116c716148abbad0843fe9dc
Zachary Shenkman <yankeedude02@gmail.com>
2022-03-26 17:56:27 -0400

    Adds search bar to home screen

--- src/main/HomeView.js
+++ src/main/HomeView.js
@@ -9,24 +9,33 @@ export default function HomeView({ navigation }) {
     const { currentUser } = React.useContext(AuthContext)
 
     const DrawerButton = () => (
-        <TouchableOpacity style={styles.button} onPress={openDrawer}>
-            <Feather name={'menu'} size={22} color={'#454545'}/>
+        <TouchableOpacity style={styles.button} activeOpacity={0.7} onPress={() => navigation.openDrawer()}>
+            <Feather name={'menu'} size={22} color={'#666666'}/>
         </TouchableOpacity>
     )
 
-    React.useLayoutEffect(() => {
+    const HeaderBar = () => (
+        <TouchableOpacity style={styles.headerBar} activeOpacity={0.7}>
+            <Feather name={'search'} size={18} color={'#666666'}/>
+            <Text style={styles.headerBarText}>Where are you going?</Text>
+        </TouchableOpacity>
+    )
+
+    React.useEffect(() => {
         navigation.setOptions({
             headerTransparent: true,
             headerLeft: DrawerButton,
+            headerTitle: HeaderBar,
+            headerTitleContainerStyle: {
+                flexGrow: 1
+            },
             headerLeftContainerStyle: {
-                padding: 15
+                padding: 15,
+                marginRight: 20,
+                flexGrow: 0
             }
         })
     }, [])
-
-    function openDrawer() {
-        navigation.openDrawer()
-    }
 
     return (
         <View style={styles.container}>
@@ -63,5 +72,27 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 5,  
     elevation: 3
+  },
+  headerBar: {
+    backgroundColor: '#fff',
+    height: 40,
+    flexGrow: 1,
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,  
+    elevation: 1
+  },
+  headerBarText: {
+      fontWeight: '500',
+      fontSize: 15,
+      fontStyle: 'italic',
+      color: 'rgba(0, 0, 0, 0.6)',
+      paddingHorizontal: 5
   }
 });
